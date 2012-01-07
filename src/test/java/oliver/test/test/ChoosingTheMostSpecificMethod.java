8f1caccbd90dc75c4d22153bb8798682867f5b5e
lichengwu <ol_l@msn.cn>
2012-01-07 11:14:37 +0800

    add test

---
+++ src/test/java/oliver/test/test/ChoosingTheMostSpecificMethod.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2010-2011 lichengwu
+ * All rights reserved.
+ * 
+ */
+package oliver.test.test;
+
+/**
+ * 
+ * @author lichengwu
+ * @created 2012-1-6
+ * 
+ * @version 1.0
+ */
+public class ChoosingTheMostSpecificMethod {
+
+	public static void method(Object obj) {
+		System.out.println("method with param type - Object");
+	}
+
+	public static void method(String obj) {
+		System.out.println("method with param type - String");
+	}
+	public static void method(Integer obj) {
+		System.out.println("method with param type - Integer");
+	}
+
+	public static void main(String[] args) {
+//		method(null);
+	}
+
+}
