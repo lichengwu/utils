9280cf5bb3c21f413ad061485ac6e0ef9d522509
lichengwu <ol_l@msn.cn>
2012-09-28 15:07:39 +0800

    for HttpSecureLinkModule

---
+++ src/main/java/oliver/http/HttpSecureLink.java
@@ -0,0 +1,43 @@
+package oliver.http;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * HttpSecureLink for nginx HttpSecureLinkModule
+ * http://wiki.nginx.org/HttpSecureLinkModule
+ * User: lichengwu
+ * Date: 9/28/12
+ * Time: 2:58 PM
+ */
+public class HttpSecureLink {
+
+    private HttpSecureLink() {
+    }
+
+    /**
+     * 生成带超时的加密串
+     *
+     * @author lichengwu
+     * @created 2012-4-16
+     *
+     * @param secret 公匙
+     * @param uri 需要加密的uri
+     * @param expire 超时时间
+     * @return 带超时的加密串
+     */
+    public static String getCode(String secret, String uri, long expire) {
+        try {
+            MessageDigest alga = MessageDigest.getInstance("MD5");
+            alga.update((secret + uri + expire).getBytes());
+            String code = new sun.misc.BASE64Encoder().encode(alga.digest());
+            code = code.replace('+', '-');
+            code = code.replace('/', '_');
+            code = code.replace("=", "");
+            return code;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
