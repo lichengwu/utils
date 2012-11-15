b64302651b2268c0d1ef9ff19566ff504e85a799
lichengwu <lichengwu@meituan.com>
2012-11-15 20:50:33 +0800

    更新Objects工具类

--- src/main/java/oliver/lang/Objects.java
+++ src/main/java/oliver/lang/Objects.java
@@ -5,14 +5,7 @@
  */
 package oliver.lang;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
-import java.io.Serializable;
+import java.io.*;
 import java.lang.reflect.Method;
 
 import net.sf.cglib.proxy.Enhancer;
@@ -29,137 +22,77 @@ import net.sf.cglib.proxy.MethodProxy;
  */
 final public class Objects {
 
-	private Objects() {
-	}
+    private Objects() {
+    }
 
-	public static Object clone(Object object) {
-		if (!(object instanceof Serializable)) {
-			Enhancer enhancer = new Enhancer();
-			enhancer.setInterfaces(new Class[] { Serializable.class });
-			enhancer.setSuperclass(object.getClass());
-			enhancer.setCallback(new MethodInterceptor() {
-				public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy)
-				        throws Throwable {
-					return proxy.invoke(obj, args);
-				}
-			});
-			object = enhancer.create();
-		}
+    /**
+     * deep clone an object by serialization/deserialization
+     * 
+     * @param object
+     * @return
+     */
+    public static <T> T clone(T object) {
+        Object newObject = object;
+        if (!(object instanceof Serializable)) {
+            Enhancer enhancer = new Enhancer();
+            enhancer.setInterfaces(new Class[] { Serializable.class });
+            enhancer.setSuperclass(object.getClass());
+            enhancer.setCallback(new MethodInterceptor() {
+                public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy)
+                        throws Throwable {
+                    return proxy.invoke(obj, args);
+                }
+            });
+            newObject = enhancer.create();
+        }
 
-		ByteArrayOutputStream baos = null;
-		ObjectOutputStream oos = null;
-		ByteArrayInputStream bais = null;
-		ObjectInputStream ois = null;
-		try {
-			baos = new ByteArrayOutputStream();
-			oos = new ObjectOutputStream(baos);
-			oos.writeObject(object);
-			bais = new ByteArrayInputStream(baos.toByteArray());
-			ois = new ObjectInputStream(bais);
-			return ois.readObject();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (Throwable e) {
-			e.printStackTrace();
-		} finally {
-			close(bais);
-			close(ois);
-			close(baos);
-			close(oos);
-		}
-		return null;
-	}
+        ByteArrayOutputStream baos = null;
+        ObjectOutputStream oos = null;
+        ByteArrayInputStream bais = null;
+        ObjectInputStream ois = null;
+        try {
+            baos = new ByteArrayOutputStream();
+            oos = new ObjectOutputStream(baos);
+            oos.writeObject(newObject);
+            bais = new ByteArrayInputStream(baos.toByteArray());
+            ois = new ObjectInputStream(bais);
+            return (T) ois.readObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (Throwable e) {
+            e.printStackTrace();
+        } finally {
+            Closer.close(bais);
+            Closer.close(ois);
+            Closer.close(baos);
+            Closer.close(oos);
+        }
+        return null;
+    }
 
-	/**
-	 * 关闭流
-	 * 
-	 * @author lichengwu
-	 * @created Aug 7, 2011
-	 * 
-	 * @param os
-	 */
-	private static void close(OutputStream os) {
-		if (os != null) {
-			try {
-				os.close();
-			} catch (IOException e) {
-			} finally {
-				os = null;
-			}
-		}
-	}
+    /**
+     * 比较对象是否相等
+     * 
+     * @param obj1
+     * @param obj2
+     * 
+     * @return 当obj1.equals(obj2)或obj1==obj2==null返回true，否则返回false
+     */
+    public static boolean equals(Object obj1, Object obj2) {
+        boolean returnValue = false;
+        if (obj1 == null && obj2 == null) {
+            returnValue = true;
+        } else if (obj1 == null && obj2 != null) {
+            returnValue = false;
+        } else if (obj1 != null && obj2 == null) {
+            returnValue = false;
+        } else {
+            returnValue = obj1.equals(obj2);
+        }
 
-	/**
-	 * 关闭流
-	 * 
-	 * @author lichengwu
-	 * @created Aug 7, 2011
-	 * 
-	 * @param is
-	 */
-	private static void close(InputStream is) {
-		if (is != null) {
-			try {
-				is.close();
-			} catch (IOException e) {
-			} finally {
-				is = null;
-			}
-		}
-	}
-
-	public static void main(String[] args) {
-		/**
-		 * class for clone test
-		 * 
-		 * @author lichengwu
-		 * @created 2012-6-16
-		 * 
-		 * @version 1.0
-		 */
-		class Oliver implements Serializable {
-
-			private static final long serialVersionUID = -5878048307879683975L;
-			private String name;
-
-			/**
-			 * @return the name
-			 */
-			public String getName() {
-				return name;
-			}
-
-			/**
-			 * @param name
-			 */
-			public void setName(String name) {
-				this.name = name;
-			}
-
-			/**
-			 * @return the age
-			 */
-			public int getAge() {
-				return age;
-			}
-
-			/**
-			 * @param age
-			 */
-			public void setAge(int age) {
-				this.age = age;
-			}
-
-			private int age;
-
-		}
-		Oliver oliver = new Oliver();
-		oliver.setAge(24);
-		oliver.setName("oliver");
-		Oliver clone = (Oliver) Objects.clone(oliver);
-		System.out.println(clone.getName() + ":" + clone.getAge());
-	}
+        return returnValue;
+    }
 
 }
